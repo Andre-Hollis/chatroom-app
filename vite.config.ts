9251db83dd109bd905491dfe1eec37d4cb3c061c
Andre Hollis <andrehollis40@gmail.com>
2025-09-15 14:30:16 +1000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  resolve: {
+    resolve: {
         alias: {
             '@': resolve(__dirname, 'src'),
         },
     },
-  plugins: [react()],
-})
+    plugins: [react()],
+});
