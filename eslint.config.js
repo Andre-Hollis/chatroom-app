1f5f0930df00fdae5902537e8088fa10a6ff3c9c
Andre Hollis <andrehollis40@gmail.com>
2025-09-15 14:24:01 +1000

    wip

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,39 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+        rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "indent": [
+        "error",
+        4,
+        {
+          "SwitchCase": 1,
+          "MemberExpression": 1,
+          // "ignoredNodes": [
+          //   "JSXElement",
+          //   "JSXElement *"
+          // ]
+        }
+      ],
+      "no-multi-spaces": "error",
+      "comma-dangle": [
+        "warn",
+        {
+          "arrays": "always-multiline",
+          "objects": "always-multiline",
+          "imports": "always-multiline",
+          "exports": "always-multiline",
+          "functions": "always-multiline"
+        }
+      ],
+      "semi": "error",
+      "quotes": [
+        "error",
+        "single",
+        "avoid-escape"
+      ],
+      "@typescript-eslint/no-non-null-assertion": "warn"
+    }
   },
 ])
